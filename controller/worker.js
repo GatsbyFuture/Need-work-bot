33d6cda8ed3b4c490e9b0a8213f1a4be9c4664fe
Raxmatov Eldorbek <raxmatoveldor37@gmail.com>
2021-08-23 21:35:10 +0500

    create new a functions

--- controller/worker.js
+++ controller/worker.js
@@ -17,7 +17,7 @@ async function controlWorkers(ctx) {
     Markup
       .keyboard([
         ['Posida moy', 'Enaga', 'O\'qituvchi'],
-        ['Uborchitsa', 'Pragramist', 'Quruvchi'],
+        ['Farrosh', 'Pragramist', 'Quruvchi'],
         ['Sotuvchi', 'Makler', 'Nonvoy'],
         ['Shafyor', 'Sport triner', 'Durodgor'],
         ['Mexanik', 'Santexnik', 'Auto moychik'],
@@ -31,131 +31,144 @@ async function controlWorkers(ctx) {
 async function controlWhatch(ctx) {
   let shoose = ctx.message.text;
   switch (shoose) {
-    case 'Posida moy': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
-        [Markup.callbackButton('Barcha ishchilar', 'worker1'),
-        Markup.callbackButton('Nomzodni qo\'yish', 'registration1')],
-        [Markup.callbackButton('Ortga qaytish', 'Back1')]
-      ])).HTML()
+    case 'Posida moy': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/posidamoy.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+        .markup(Markup.inlineKeyboard([
+          [Markup.callbackButton('Barcha ishchilar', 'worker1'),
+          Markup.callbackButton('Nomzodni qo\'yish', 'registration1')],
+          [Markup.callbackButton('Ortga qaytish', 'Back1')]
+        ])).HTML()
     ); break;
-    case 'Enaga': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Enaga': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/enaga.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker2'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration2')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'O\'qituvchi': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'O\'qituvchi': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/o\'qituvchi.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker3'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration3')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Uborchitsa': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Farrosh': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/farrosh.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker4'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration4')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Pragramist': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Pragramist': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/pragramist.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker5'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration5')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Quruvchi': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Quruvchi': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/quruvchi.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker6'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration6')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Sotuvchi': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Sotuvchi': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/sotuvchi.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker7'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration7')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Makler': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Makler': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/makler.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker8'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration8')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Nonvoy': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Nonvoy': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/nonvoy.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker9'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration9')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Shafyor': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Shafyor': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/shafyor.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker10'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration10')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Sport triner': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Sport triner': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/triner.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker11'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration11')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Durodgor': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Durodgor': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/duradgor.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker12'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration12')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Mexanik': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Mexanik': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/mexanik.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker13'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration13')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Santexnik': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Santexnik': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/santexnik.jpg' },
+      Extra.caption('<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker14'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration14')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
-    case 'Auto moychik': await ctx.replyWithHTML(
-      '<b>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>',
-      Extra.markup(Markup.inlineKeyboard([
+    case 'Auto moychik': await ctx.replyWithPhoto(
+      { source: './media/photoCategoriesWorker/automoychik.jpg' },
+      Extra.caption('<b><i>Auto moychik</i>Ro\'yxatdan o\'tish yoki ro\'yxatni ko\'rish</b>')
+      .markup(Markup.inlineKeyboard([
         [Markup.callbackButton('Barcha ishchilar', 'worker15'),
         Markup.callbackButton('Nomzodni qo\'yish', 'registration15')],
         [Markup.callbackButton('Ortga qaytish', 'Back1')]
       ])).HTML()
     ); break;
   }
+  ctx.deleteMessage();
 }
-
-
-
 module.exports = {
   controlStart,
   controlWorkers,
