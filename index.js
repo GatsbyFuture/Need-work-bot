0ed0d0c8581d4a0a06cdf1ed98f13f499ead87d1
Raxmatov Eldorbek <raxmatoveldor37@gmail.com>
2021-08-23 21:54:20 +0500

    install the npm package

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
 const { controlStart, controlWorkers, controlWhatch } = require('./controller/worker');
-
 require('dotenv').config({ path: './environment/parols.env' });
-
+const fs = require('fs');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 // midllewares...
 bot.use(Telegraf.log());
+// bot.use(session());
 
 // boshlang'ich tanlov paneli uchun button...
 bot.start(async (ctx) => {
   await controlStart(ctx);
 });
+// Faqat ishchilar uchun function
 // Umumiy ishchilar ro'yxati...
 bot.action('need', async (ctx) => {
   await controlWorkers(ctx);
   // return ctx.answerCbQuery(`N ta ishchi mavjud`);
 });
-// tanlangan ishchilar ro'yxati va registratsa qilish...
+// tanlangan ishchilar yoki nomzod qo'yish bo'lmimi?
 bot.on('message', async (ctx) => {
   await controlWhatch(ctx);
 });
@@ -28,7 +29,16 @@ bot.action('Back1', async (ctx) => {
   await controlWorkers(ctx);
 });
 
-// asasiy ishchilar bo'limi yoki nomzod qo'yish bo'lmimi?
+// ro'yxatga olish jarayoni...
+bot.action('worker1', (ctx) => {
+  fs.readFile('./dataText/AllWorkers.txt', 'utf8', (err, result) =>{
+    if(err)
+    console.log(err);
+    else
+    ctx.reply(result); 
+  });
+});
+
 
 
 console.log('bot ishga tushdi');
