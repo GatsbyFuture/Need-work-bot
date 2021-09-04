cc5713d7a827f955ee03bd2b799a243c7af457e5
Raxmatov Eldorbek <raxmatoveldor37@gmail.com>
2021-09-04 18:49:01 +0500

    the field was opened for a new document

--- db/connect.js
+++ db/connect.js
@@ -51,6 +51,49 @@ const userDataSchema = new mongoose.Schema({
     default: Date.now
   }
 }, { collection: 'tb_user_data' });
+const userDataWork = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  payment_type: {
+    type: String,
+    required: true,
+  },
+  payment_amount:{
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    minleng: 10
+  },
+  telNumber: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: Boolean,
+    default: true
+  },
+  workType: String,
+  chat_id: Number,
+  stay_time: {
+    type: Number,
+    default: 1
+  },
+  dataTime: {
+    type: Date,
+    default: Date.now
+  }
+},{collection:'tb_userWork_data'});
 // userni botdan olingan datasi uchun schema..
 const userDataBotSchema = new mongoose.Schema({
   chat_id: Number,
@@ -64,8 +107,10 @@ const userDataBotSchema = new mongoose.Schema({
 // schemalardan obj olibb olamiz va export qilamiz..
 const tbUser = mongoose.model('tb_user_data', userDataSchema);
 const tbBot = mongoose.model('tb_bot_data', userDataBotSchema);
+const tbUserWork = mongoose.model('tb_userWork_data', userDataWork);
 // import qilib chiqarib qo'yamiz foydala
 module.exports = {
   tbUser,
-  tbBot
+  tbBot,
+  tbUserWork
 };
