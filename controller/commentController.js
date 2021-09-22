e5360ab5ac120ba8666e654209e5f02ab6313558
Raxmatov Eldorbek <raxmatoveldor37@gmail.com>
2021-09-22 15:37:14 +0500

    errors have been corrected

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,24 +1,29 @@
 // all category buttons
-const Extra = require('telegraf/extra');
-const Markup = require('telegraf/markup');
+const Extra = require("telegraf/extra");
+const Markup = require("telegraf/markup");
 // izox qoldirish uchun namuna...
 async function commentWord(ctx) {
- ctx.replyWithPhoto({ source: './media/NamunagaRasm/Just_think.jpg' },
-  Extra.caption('Bu botda yana qanday bo\'limlar ochilishini xoxlaysiz, ' +
-   '<i>bu haqda yozib qoldirish mumkin</i>😊😊😊')
-   .HTML()
- );
+    ctx.replyWithPhoto(
+        { source: "./media/NamunagaRasm/Just_think.jpg" },
+        Extra.caption(
+            "Bu botda yana qanday bo'limlar ochilishini xoxlaysiz, " +
+                "<i>bu haqda yozib qoldirish mumkin</i>😊😊😊"
+        ).HTML()
+    );
 }
 async function thinkCom(ctx) {
- ctx.replyWithHTML('<b>Komentariya saqlansinmi?</b>',
-  Extra.markup(Markup.inlineKeyboard([
-   [Markup.callbackButton('Bekor qilish', 'cancel')],
-   [Markup.callbackButton('Yuborish', 'enter')]
-  ]))
- );
+    ctx.replyWithHTML(
+        "<b>Komentariya saqlansinmi?</b>",
+        Extra.markup(
+            Markup.inlineKeyboard([
+                [Markup.callbackButton("Bekor qilish", "cancel")],
+                [Markup.callbackButton("Yuborish", "enter")],
+            ])
+        )
+    );
 }
 
 module.exports = {
- commentWord,
- thinkCom
-}
+    commentWord,
+    thinkCom,
+};
