7f1f820fefe24362060612ddbe8c64f0add96da9
Raxmatov Eldorbek <raxmatoveldor37@gmail.com>
2021-09-23 11:49:52 +0500

    added new check function

--- controllErrors/chackNumber.js
+++ controllErrors/chackNumber.js
@@ -3,15 +3,17 @@ async function isItNumber(params) {
     // nomer kelganda orqa oldida yoki o'rtasida "-" yoki " " joylarni tozlash.
     const x = params.trim().replace(/ /g, "");
     if (/^\+998\d{8,9}$/.test(x)) {
-        // console.log("bor");
-        // console.log("bor1");
         const c = x.substr(0, 4) + "(" + x.substr(4);
         const v = c.substr(0, 7) + ")" + c.substr(7);
         const b = v.substr(0, 13) + "-" + c.substr(12);
         const n = b.substr(0, 11) + "-" + b.substr(11);
         // console.log(n);
         return { result: true, data: n };
-    } else if (/^9\d{8,9}$/.test(x) || /^8\d{8,9}$/ || /^6\d{8,9}$/) {
+    } else if (
+        /^9\d{8,9}$/.test(x) ||
+        /^8\d{8,9}$/.test(x) ||
+        /^6\d{8,9}$/.test(x)
+    ) {
         const q = "+998" + x;
         // console.log(q);
         const c = q.substr(0, 4) + "(" + q.substr(4);
